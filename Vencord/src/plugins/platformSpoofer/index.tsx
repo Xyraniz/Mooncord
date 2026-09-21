df3c1ae5725e64a8ff9236073b8568db1cb499a9
Xyraniz <xyraniz@protonmail.com>
2026-09-21 00:46:06 -0600

    feat: add platform spoofer plugin

---
+++ Vencord/src/plugins/platformSpoofer/index.tsx
@@ -0,0 +1,84 @@
+/*
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2022 Vendicated and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import "./style.css";
+
+import { definePluginSettings } from "@api/Settings";
+import { Devs } from "@utils/constants";
+import definePlugin, { OptionType } from "@utils/types";
+import { Forms } from "@webpack/common";
+
+const settings = definePluginSettings({
+    platform: {
+        type: OptionType.SELECT,
+        description: "What platform to show up as on",
+        restartNeeded: true,
+        options: [
+            {
+                label: "Desktop",
+                value: "desktop",
+                default: true,
+            },
+            {
+                label: "Web",
+                value: "web",
+            },
+            {
+                label: "Mobile",
+                value: "mobile",
+            },
+            {
+                label: "Embedded (Console)",
+                value: "embedded",
+            },
+        ]
+    }
+});
+
+export default definePlugin({
+    name: "PlatformSpoofer",
+    description: "Spoof what platform or device you're on",
+    authors: [Devs.Keira],
+    settingsAboutComponent: () => <>
+        <Forms.FormText className="platform-warning">
+            I can't guarantee this plugin won't get you warned or banned.
+        </Forms.FormText>
+    </>,
+    settings,
+    patches: [
+        {
+            find: "_doIdentify(){",
+            replacement: {
+                match: /(\[IDENTIFY\].*let.{0,5}=\{.*properties:)(.*),presence/,
+                replace: "$1{...$2,...$self.getPlatform()},presence"
+            }
+        }
+    ],
+    getPlatform: () => {
+        switch (settings.store.platform ?? "desktop") {
+            case "desktop":
+                return { browser: "Discord Client" };
+            case "web":
+                return { browser: "Chrome" };
+            case "mobile":
+                return { browser: "Discord iOS" };
+            case "embedded":
+                return { browser: "Discord Embedded" };
+        }
+    }
+});
